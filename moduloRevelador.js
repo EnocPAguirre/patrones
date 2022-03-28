8e831df833074897a6884494c661b1169a2e7fb4
EnocPAguirre <enocpinedaa@gmail.com>
2022-03-27 22:37:43 -0600

    Patron Modulo Revelador

---
+++ moduloRevelador.js
@@ -0,0 +1,13 @@
+const resultado = (()=> {
+
+    const x = {};
+
+    return {
+        a: () => console.log(x),
+        b: (key, val) => x[key] = val
+    }
+
+})();
+
+resultado.a();
+resultado.b('patina', 'linea');
