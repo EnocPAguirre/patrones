66d58695e89507639ad63992a527cfee05531783
EnocPAguirre <enocpinedaa@gmail.com>
2022-03-30 22:24:04 -0600

    mejorando compsicion

--- composicion.js
+++ composicion.js
@@ -1,15 +1,24 @@
 const user = [
-    {nombre: 'Enoc', apellido: 'Pineda', edad: 24},
-    {nombre: 'Pineda', apellido: 'Aguirre', edad: 23},
-]
+  { nombre: "Enoc", apellido: "Pineda", edad: 24 },
+  { nombre: "Pineda", apellido: "Aguirre", edad: 23 },
+];
 
-const head = xs => xs[0];
-const formateo = x => ({
-    nombreCompleto: `${x.nombre} ${x.apellido}`,
-    edad: x.edad
-})
+const compose =
+  (...fns) =>
+  (x) =>
+    fns.reduceRight((y, f) => f(y), x);
+const head = (xs) => xs[0];
+const filter = (f) => xs.filter(f);
+const formateo = (x) => ({
+  nombreCompleto: `${x.nombre} ${x.apellido}`,
+  edad: x.edad,
+});
 
-const formato = x => `${x.nombreCompleto} tiene ${x.edad}`
+const formato = (x) => `${x.nombreCompleto} tiene ${x.edad}`;
 
-const traerPrimerInfante = data => formato(formateo(head(data.filter(x => x.edad < 24))));
-
+const traerPrimerInfante = compose(
+  formato,
+  formateo,
+  head,
+  filter((x) => x.edad < 24)
+);
